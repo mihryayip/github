1504ab12548a7744f1994a348a6e5ba85adc5733
mihryayip <83121087+mihryayip@users.noreply.github.com>
2021-05-08 15:31:47 +0300

    Update misha.py

--- misha.py
+++ misha.py
@@ -5,7 +5,20 @@ w = 700
 h = 500
 
 win = display.set_mode((w,h))
+class Alpha(sprite.Sprite):
+    def __init__(self,x,y,h,w,image_name,speed):
+        super().__init__()
+        self.image = image.load(image_name)
+        self.rect = Rect(w,h,y,x)
+    def reset(self):
+        win.blit(self.image,(self.rect.x,self.rect.y))
+
+ball = Alpha(550,550,)
+
+
 win.fill((255,255,255))
+
+
 
 while True:
 
